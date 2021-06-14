493a381763f13c495f151aec4816d5617cb86385
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-06-14 08:46:02 -0600

    snake part 2 complete

--- snakeGamePt2/food.py
+++ snakeGamePt2/food.py
@@ -11,6 +11,9 @@ class Food(Turtle):
         self.shapesize(0.5, 0.5)
         self.color("blue")
         self.speed("fastest")
+        self.refresh_location()
+
+    def refresh_location(self):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
---
+++ snakeGamePt2/scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 16, "normal")
+
+
+# TODO create a new scoreboard class that inherits from the Turtle class
+# turtle.write, turtle.clear
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 270)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.clear()
+        self.score += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game over", align=ALIGNMENT, font=FONT)
+
