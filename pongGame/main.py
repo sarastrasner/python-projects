1a1059b5bdff3c858db80e7280b2f0aed07fb3e9
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-06-15 09:46:20 -0600

    ball functional

--- pongGame/main.py
+++ pongGame/main.py
@@ -1,5 +1,8 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor("black")
@@ -10,6 +13,8 @@ screen.listen()
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
 
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
@@ -18,7 +23,29 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(ball.move_speed)
     screen.update()
+    ball.move()
+
+    # TODO detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        # TODO ball should bounce
+        ball.bounce_y()
+
+    # TODO Detect collision with right paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or \
+            ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    # TODO Detect when right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # TODO Detect when left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 
 screen.exitonclick()
