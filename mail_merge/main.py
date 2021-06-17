ce25f94ccf0a10fea861c31ec01026420d1b0852
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-06-17 14:08:45 -0600

    initial commit mail merge project

---
+++ mail_merge/main.py
@@ -0,0 +1,10 @@
+# TODO: Create a letter using starting_letter.txt
+# TODO: for each name in invited_names.txt
+# TODO: Replace the [name] placeholder with the actual name.
+# TODO: Save the letters in the folder "ReadyToSend".
+# letter_for_[person's name]
+
+
+# TODO: Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# TODO: Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# TODO: Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
