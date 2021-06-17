4aca07d5e5c556b17c7f57cfa656106608fe9670
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-06-17 14:50:04 -0600

    readme done

---
+++ mail_merge/README.md
@@ -0,0 +1,8 @@
+# Mail Merge Project
+Opening, reading, and writing files to complete a mail merge project. Merged letters are in the ReadyToSend folder.
+
+# Feature Tasks
+- Create a letter using `starting_letter.txt`
+- Replace the `[name]` placeholder with the actual name.
+- for each name in `invited_names.txt`, save the letters in the folder "ReadyToSend" with a filename `letter_for_[person's name].txt`.
+       
--- mail_merge/main.py
+++ mail_merge/main.py
@@ -1,25 +1,21 @@
+PLACEHOLDER = "[name]"
+
 # TODO: Create a letter using starting_letter.txt
-# TODO: for each name in invited_names.txt
-# TODO: Replace the [name] placeholder with the actual name.
-# TODO: Save the letters in the folder "ReadyToSend".
-# letter_for_[person's name]
-
-# TODO: Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-# TODO: Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-# TODO: Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
-
 with open('./Input/Letters/starting_letter.txt', mode="r") as letter:
     contents = letter.read()
-    # TODO: Do the replacing here
-    with open('./Input/Names/invited_names.txt') as file:
-        names = file.readlines()
-        for name in names:
-            stripped_name = name.strip()
-            file_name = stripped_name.replace(" ", "_")
-            new_letter = contents.replace("[name]", f"{stripped_name}")
-            if file_name:
-                with open(f"letter_for_{file_name}.txt", mode="w") as new_file:
-                    new_file.write(f"{new_letter}")
-            else:
-                with open(f"letter_for_{stripped_name}.txt", mode="w") as new_file:
-                    new_file.write(f"{new_letter}")
+
+# TODO: Replace the [name] placeholder with the actual name.
+with open('./Input/Names/invited_names.txt') as file:
+    names = file.readlines()
+    # TODO: for each name in invited_names.txt
+    for name in names:
+        stripped_name = name.strip()
+        file_name = stripped_name.replace(" ", "_")
+        new_letter = contents.replace(PLACEHOLDER, stripped_name)
+        # TODO: Save the letters in the folder "ReadyToSend" with a filename of "letter_for_[person's name]".
+        if file_name:
+            with open(f"./Output/ReadyToSend/letter_for_{file_name}.txt", mode="w") as new_file:
+                new_file.write(f"{new_letter}")
+        else:
+            with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as new_file:
+                new_file.write(f"{new_letter}")
