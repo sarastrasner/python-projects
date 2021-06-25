f30e48e8d43ef3fbe67620f15f34027bdf3a42da
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-06-25 08:54:36 -0600

    updated phonetic alphabet to handle errors

--- nato_alphabet/main.py
+++ nato_alphabet/main.py
@@ -6,6 +6,18 @@ data = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word_to_translate = input("Enter a word: ").upper()
-output_list = [nato_dict[letter] for letter in word_to_translate]
-print(output_list)
+# TODO: Address the error that would occur if the user enters non-letter characters
+
+
+def generate_phonetic():
+    word_to_translate = input("Enter a word: ").upper()
+    try:
+        output_list = [nato_dict[letter] for letter in word_to_translate]
+    except KeyError:
+        print("Sorry, only letters in the alphabet, please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
