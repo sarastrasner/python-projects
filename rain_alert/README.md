9cd37816fee043d5f367823178ff08dc3eedec71
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-07-07 10:24:07 -0600

    documentation done

--- rain_alert/README.md
+++ rain_alert/README.md
@@ -3,3 +3,8 @@ Checks the weather forecast for a given location, and sends me a text message if
 Uses Twilio for sending SMS and the OpenWeather API for checking the forecast.
 
 # Feature Tasks
+1. Register with the OpenWeather API and use your API key to get the weather forecast for the next 12 hours.
+1. If it will rain in the next 12 hours, print "Bring an umbrella"
+1. Instead of printing, send a text message via Twilio. 
+1. Save all keys and sensitive information as environmental variables.
+1. Deploy this code and run it every morning with PythonAnywhere
--- rain_alert/main.py
+++ rain_alert/main.py
@@ -2,18 +2,20 @@ import requests
 from twilio.rest import Client
 import os
 
-parameters = {
-    "lat": 39.739235,
-    "lon": -104.990250,
-    "exclude": "current,minutely,daily",
-    "appid": "3bb4547d22ba25d1c922274a905ae485"
-}
 
 OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
-account_sid = os.environ.get('OWM_KEY')
+owm_key = os.environ.get('OWM_KEY')
 auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
 my_number = os.environ.get('MY_NUMBER')
+parameters = {
+    "lat": 39.739235,
+    "lon": -104.990250,
+    "appid": owm_key,
+    "exclude": "current,minutely,daily"
+
+}
+
 response = requests.get(OWM_Endpoint, params=parameters)
 response.raise_for_status()
 weather_data = response.json()
