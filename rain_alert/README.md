b0c177268e66fff1d47302e7b942a955c18d8dff
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-07-07 10:08:28 -0600

    all code functional

---
+++ rain_alert/README.md
@@ -0,0 +1,5 @@
+# Rain Alert
+Checks the weather forecast for a given location, and sends me a text message if it will rain within the next 12 hours. 
+Uses Twilio for sending SMS and the OpenWeather API for checking the forecast.
+
+# Feature Tasks
--- rain_alert/main.py
+++ rain_alert/main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
+import requests
+from twilio.rest import Client
+import os
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+parameters = {
+    "lat": 39.739235,
+    "lon": -104.990250,
+    "exclude": "current,minutely,daily",
+    "appid": "3bb4547d22ba25d1c922274a905ae485"
+}
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+account_sid = os.environ.get('TWILIO_ACCOUNT_SID')
+account_sid = os.environ.get('OWM_KEY')
+auth_token = os.environ.get('TWILIO_AUTH_TOKEN')
+my_number = os.environ.get('MY_NUMBER')
+response = requests.get(OWM_Endpoint, params=parameters)
+response.raise_for_status()
+weather_data = response.json()
+weather_slice = weather_data['hourly'][:12]
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = hour_data['weather'][0]['id']
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Bring an ☂️.",
+        from_='+16789819663',
+        to=my_number
+    )
+
+    print(message.status)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
