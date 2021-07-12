909d6a48632ec59b8495227d4daca56ac02e7cfc
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-07-12 12:19:31 -0600

    habit tracker done

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -1,16 +1,58 @@
-# This is a sample Python script.
+import requests
+import datetime
+USERNAME = "strassy"
+TOKEN = "jhgjhgjg8o773"
+GRAPH_ID = "graph1"
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+pixela_parameters = {
+    "token": "jhgjhgjg8o773",
+    "username": "strassy",
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+pixela_endpoint = 'https://pixe.la/v1/users'
+
+# CREATING A USER
+# response = requests.post(url=pixela_endpoint, json=pixela_parameters)
+# print(response.text)
+
+# CREATING A GRAPH
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_parameters = {
+    "id": "graph1",
+    "name": "Coding Graph",
+    "unit": "minutes",
+    "type": "int",
+    "color": "ajisai"
+}
+headers_dict = {"X-USER-TOKEN": TOKEN}
+# response = requests.post(url=graph_endpoint, json=graph_parameters, headers=headers_dict)
+# print(response.text)
+# Graph is live at https://pixe.la/v1/users/strassy/graphs/graph1.html
+
+# ADDING AN EVENT TO TRACK
+today = datetime.datetime.today().strftime('%Y%m%d')
+
+post_parameters = {
+    "date": today,
+    "quantity": input("How many minutes did you code today? "),
+}
+
+post_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}"
+
+response = requests.post(url=post_endpoint, json=post_parameters, headers=headers_dict)
+print(response.text)
+
+# PUT
+# response = requests.put(url=update_endpoint, json=new_pixel_data, headers=headers)
+# print(response.text)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# delete_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{GRAPH_ID}/{today.strftime('%Y%m%d')}"
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# DELETE
+# response = requests.delete(url=delete_endpoint, headers=headers)
+# print(response.text)
