b12a9dfc67721984172b6a921739da2c255f100c
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-08-02 14:13:51 -0600

    ycombinator scraper done

--- movieWatchlist/README.md
+++ movieWatchlist/README.md
@@ -1,2 +1,2 @@
 # Movie Watchlist
-a list of movies to watch. Data is scraped from Empire's list of 100 best movies of all time using BeautifulSoup.
+A list of must-see movies built using data scraped from Empire's list of 100 best movies of all time via BeautifulSoup.
--- movieWatchlist/main.py
+++ movieWatchlist/main.py
@@ -1,7 +1,50 @@
 from bs4 import BeautifulSoup
+import requests
 
-with open("website.html") as file:
-    contents = file.read()
+response = requests.get('https://news.ycombinator.com/news')
+yc_webpage = response.text
 
+soup = BeautifulSoup(yc_webpage, "html.parser")
+articles = soup.find_all(name="a", class_="storylink")
+article_texts = []
+article_links = []
 
-soup = BeautifulSoup(contents, 'html.parser')
+for article_tag in articles:
+    text = article_tag.get_text()
+    article_texts.append(text)
+    link = article_tag.get("href")
+    article_links.append(link)
+
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name="span", class_="score")]
+
+# print(article_texts)
+# print(article_links)
+# print(article_upvotes)
+
+largest_number = max(article_upvotes)
+largest_index = article_upvotes.index(largest_number)
+print(largest_index)
+
+# with open("website.html") as file:
+#     contents = file.read()
+#
+#
+# soup = BeautifulSoup(contents, 'html.parser')
+# # print(soup.title)
+# all_anchor_tags = soup.find_all(name="a")
+#
+#
+# for tag in all_anchor_tags:
+#     #print(tag.getText())
+#     # print(tag.get("href"))
+#     pass
+#
+# heading = soup.find(name="h1", id="name")
+# print(heading)
+#
+# section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading)
+#
+# soup.find_all("a")
+#
+# soup.select_one(selector="p a")
