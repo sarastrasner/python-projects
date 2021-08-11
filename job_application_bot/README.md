1057dcbcbd2bac0fe83104578600b8ce20b47622
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-08-11 14:15:08 -0600

    successfully able to apply for first listed job in URL

--- job_application_bot/README.md
+++ job_application_bot/README.md
@@ -1,3 +1,8 @@
 # Job Application Bot
+Uses Selenium to automate applying for jobs via LinkedIn.
 
 ## Feature Tasks
+1. Set up your LinkedIn account and upload a resume to your account
+1. Get the URL of your desired job search and use Selenium to access that page
+   1. You will need to automate signing in to LinkedIn before accessing the job search page.
+1. Use Selenium to automatically apply to the first job that only requires you to enter your phone number.
--- job_application_bot/main.py
+++ job_application_bot/main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import os
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+ACCOUNT_EMAIL = os.environ['ACCOUNT_EMAIL']
+ACCOUNT_PASSWORD = os.environ['ACCOUNT_PASSWORD']
+URL = 'https://www.linkedin.com/jobs/search/?f_AL=true&f_WRA=true&geoId=103644278&keywords=associate%20software' \
+      '&location=United%20States '
+PHONE = '225-368-5354'
 
+chrome_driver_path = "/Users/sarastrasner/Development/chromedriver"
+driver = webdriver.Chrome(chrome_driver_path)
+driver.get(URL)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+sign_in_button = driver.find_element_by_link_text("Sign in")
+sign_in_button.click()
 
+time.sleep(5)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+email_field = driver.find_element_by_id("username")
+email_field.send_keys(ACCOUNT_EMAIL)
+password_field = driver.find_element_by_id("password")
+password_field.send_keys(ACCOUNT_PASSWORD)
+password_field.send_keys(Keys.ENTER)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+time.sleep(5)
+apply_button = driver.find_element_by_css_selector(".jobs-s-apply button")
+apply_button.click()
+print('Clicked on the apply button.')
+
+# Review the application
+time.sleep(5)
+next_button = driver.find_element_by_css_selector("footer div button")
+next_button.click()
+print('Clicked on the next button.')
+
+time.sleep(5)
+footer_buttons = driver.find_elements_by_css_selector("footer div button")
+review_button = footer_buttons[1]
+review_button.click()
+print('Clicked on the review button.')
+
+# Submit the application
+footer_buttons2 = driver.find_elements_by_css_selector("footer div button")
+submit_button = footer_buttons2[1]
+submit_button.click()
+print('Clicked on the submit button.')
+
+driver.quit()
