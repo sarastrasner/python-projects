7b5ab7e617e44aec52d10197cd012677d7fe31f4
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-08-11 15:04:14 -0600

    able to apply for multiple keywords

--- job_application_bot/README.md
+++ job_application_bot/README.md
@@ -1,8 +1,9 @@
 # Job Application Bot
-Uses Selenium to automate applying for jobs via LinkedIn.
+A bot that uses Selenium to automate applying for jobs via LinkedIn.
 
 ## Feature Tasks
 1. Set up your LinkedIn account and upload a resume to your account
 1. Get the URL of your desired job search and use Selenium to access that page
    1. You will need to automate signing in to LinkedIn before accessing the job search page.
 1. Use Selenium to automatically apply to the first job that only requires you to enter your phone number.
+1. Use Selenium to apply to all the jobs on the page.
--- job_application_bot/main.py
+++ job_application_bot/main.py
@@ -1,72 +1,79 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 import os
 
 ACCOUNT_EMAIL = os.environ['ACCOUNT_EMAIL']
 ACCOUNT_PASSWORD = os.environ['ACCOUNT_PASSWORD']
-URL = 'https://www.linkedin.com/jobs/search/?f_AL=true&f_WRA=true&geoId=103644278&keywords=associate%20software' \
-      '&location=United%20States '
-PHONE = '225-368-5354'
+
+keyword_list = ['software%20engineer', 'associate%20software', 'software%20engineer%20intern', 'frontend%20developer', 'react%20developer']
 
 chrome_driver_path = "/Users/sarastrasner/Development/chromedriver"
 driver = webdriver.Chrome(chrome_driver_path)
-driver.get(URL)
 
-sign_in_button = driver.find_element_by_link_text("Sign in")
-sign_in_button.click()
 
-time.sleep(5)
+def apply(query):
+    driver.get(f'https://www.linkedin.com/jobs/search/?f_AL=true&f_WRA=true&geoId=103644278&keywords={query}&location=United%20States')
 
-email_field = driver.find_element_by_id("username")
-email_field.send_keys(ACCOUNT_EMAIL)
-password_field = driver.find_element_by_id("password")
-password_field.send_keys(ACCOUNT_PASSWORD)
-password_field.send_keys(Keys.ENTER)
+    sign_in_button = driver.find_element_by_link_text("Sign in")
+    sign_in_button.click()
 
-all_listings = driver.find_elements_by_css_selector(".job-card-container--clickable")
-# all_listings = driver.find_elements_by_class_name("jobs-search-results__list-item")
+    time.sleep(5)
 
-for listing in all_listings:
-    print("found a job")
-    listing.click()
-    time.sleep(2)
+    email_field = driver.find_element_by_id("username")
+    email_field.send_keys(ACCOUNT_EMAIL)
+    password_field = driver.find_element_by_id("password")
+    password_field.send_keys(ACCOUNT_PASSWORD)
+    password_field.send_keys(Keys.ENTER)
 
-    # Try to locate the apply button, if can't locate then skip the job.
-    try:
-        apply_button = driver.find_element_by_css_selector(".jobs-s-apply button")
-        apply_button.click()
-        time.sleep(5)
+    all_listings = driver.find_elements_by_css_selector(".job-card-container--clickable")
+    # all_listings = driver.find_elements_by_class_name("jobs-search-results__list-item")
 
-        # If phone field is empty, then fill your phone number.
-        phone = driver.find_element_by_class_name("fb-single-line-text__input")
-        if phone.text == "":
-            phone.send_keys(PHONE)
+    for listing in all_listings:
+        actions = ActionChains(driver)
+        actions.move_to_element(listing).perform()
+        listing.click()
+        time.sleep(2)
 
-        submit_button = driver.find_element_by_css_selector("footer button")
+        # Try to locate the apply button, if can't locate then skip the job.
+        try:
+            apply_button = driver.find_element_by_css_selector(".jobs-s-apply button")
+            apply_button.click()
+            time.sleep(5)
 
-        # If the submit_button is a "Next" button, then this is a multi-step application, so skip.
-        if submit_button.get_attribute("data-control-name") == "continue_unify":
+            submit_button = driver.find_element_by_css_selector("footer button")
+
+            # If the submit_button is a "Next" button, then this is a multi-step application, so skip.
+            if submit_button.get_attribute("data-control-name") == "continue_unify":
+                close_button = driver.find_element_by_class_name("artdeco-modal__dismiss")
+                close_button.click()
+                time.sleep(2)
+                discard_button = driver.find_element_by_css_selector(
+                    ".artdeco-modal__actionbar .artdeco-button--primary")
+                discard_button.click()
+                print("Complex application, skipped.")
+                continue
+            else:
+                submit_button.click()
+                print('Application submitted!')
+
+            # Once application completed, close the pop-up window.
+            time.sleep(2)
             close_button = driver.find_element_by_class_name("artdeco-modal__dismiss")
             close_button.click()
-            time.sleep(2)
-            discard_button = driver.find_element_by_css_selector(".artdeco-modal__actionbar .artdeco-button--primary")
-            discard_button.click()
-            print("Complex application, skipped.")
+
+        # If already applied to job or job is no longer accepting applications, then skip.
+        except NoSuchElementException:
+            print("No application button, skipped.")
             continue
-        else:
-            submit_button.click()
 
-        # Once application completed, close the pop-up window.
-        time.sleep(2)
-        close_button = driver.find_element_by_class_name("artdeco-modal__dismiss")
-        close_button.click()
+    time.sleep(5)
+    driver.quit()
 
-    # If already applied to job or job is no longer accepting applications, then skip.
-    except NoSuchElementException:
-        print("No application button, skipped.")
-        continue
 
-time.sleep(5)
-driver.quit()
+for keyword in keyword_list:
+    apply(keyword)
+
+
