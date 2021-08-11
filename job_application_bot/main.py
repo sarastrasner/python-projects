526519064a94595070f9fcda7086ffb5ed0c8d88
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-08-11 14:38:08 -0600

    applying for multiple jobs

--- job_application_bot/main.py
+++ job_application_bot/main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
 import time
 import os
 
@@ -24,27 +25,48 @@ password_field = driver.find_element_by_id("password")
 password_field.send_keys(ACCOUNT_PASSWORD)
 password_field.send_keys(Keys.ENTER)
 
+all_listings = driver.find_elements_by_css_selector(".job-card-container--clickable")
+# all_listings = driver.find_elements_by_class_name("jobs-search-results__list-item")
+
+for listing in all_listings:
+    print("found a job")
+    listing.click()
+    time.sleep(2)
+
+    # Try to locate the apply button, if can't locate then skip the job.
+    try:
+        apply_button = driver.find_element_by_css_selector(".jobs-s-apply button")
+        apply_button.click()
+        time.sleep(5)
+
+        # If phone field is empty, then fill your phone number.
+        phone = driver.find_element_by_class_name("fb-single-line-text__input")
+        if phone.text == "":
+            phone.send_keys(PHONE)
+
+        submit_button = driver.find_element_by_css_selector("footer button")
+
+        # If the submit_button is a "Next" button, then this is a multi-step application, so skip.
+        if submit_button.get_attribute("data-control-name") == "continue_unify":
+            close_button = driver.find_element_by_class_name("artdeco-modal__dismiss")
+            close_button.click()
+            time.sleep(2)
+            discard_button = driver.find_element_by_css_selector(".artdeco-modal__actionbar .artdeco-button--primary")
+            discard_button.click()
+            print("Complex application, skipped.")
+            continue
+        else:
+            submit_button.click()
+
+        # Once application completed, close the pop-up window.
+        time.sleep(2)
+        close_button = driver.find_element_by_class_name("artdeco-modal__dismiss")
+        close_button.click()
+
+    # If already applied to job or job is no longer accepting applications, then skip.
+    except NoSuchElementException:
+        print("No application button, skipped.")
+        continue
+
 time.sleep(5)
-apply_button = driver.find_element_by_css_selector(".jobs-s-apply button")
-apply_button.click()
-print('Clicked on the apply button.')
-
-# Review the application
-time.sleep(5)
-next_button = driver.find_element_by_css_selector("footer div button")
-next_button.click()
-print('Clicked on the next button.')
-
-time.sleep(5)
-footer_buttons = driver.find_elements_by_css_selector("footer div button")
-review_button = footer_buttons[1]
-review_button.click()
-print('Clicked on the review button.')
-
-# Submit the application
-footer_buttons2 = driver.find_elements_by_css_selector("footer div button")
-submit_button = footer_buttons2[1]
-submit_button.click()
-print('Clicked on the submit button.')
-
 driver.quit()
