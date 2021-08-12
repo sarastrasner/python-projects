81ba6705270bb06145cac46d89727752b1bd0f98
SARA STRASNER <SARASTRASNER@GMAIL.COM>
2021-08-12 12:08:50 -0600

    feature tasks complete

---
+++ tinder_swipe_bot/main.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import ElementClickInterceptedException, NoSuchElementException
+from time import sleep
+
+FB_LOGIN = 'test'
+FB_PASSWORD = 'TEST'
+
+chrome_driver_path = "/Users/sarastrasner/Development/chromedriver"
+driver = webdriver.Chrome(executable_path=chrome_driver_path)
+
+driver.get('http://www.tinder.com')
+
+# Click the login button
+sleep(2)
+login_button = driver.find_element_by_xpath(
+    '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/header/div[1]/div[2]/div/button')
+login_button.click()
+
+# Click the login with FB button
+sleep(2)
+fb_login = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[1]/div/div[3]/span/div[2]/button')
+fb_login.click()
+
+# Switch to the login popup
+sleep(2)
+base_window = driver.window_handles[0]
+fb_login_window = driver.window_handles[1]
+driver.switch_to.window(fb_login_window)
+print(driver.title)
+
+# Login with FB
+email = driver.find_element_by_xpath('//*[@id="email"]')
+password = driver.find_element_by_xpath('//*[@id="pass"]')
+email.send_keys(FB_LOGIN)
+password.send_keys(FB_PASSWORD)
+password.send_keys(Keys.ENTER)
+
+# Switch back to the base window
+driver.switch_to.window(base_window)
+print(driver.title)
+
+# Allow cookies and location sharing, and turn off notifications.
+sleep(5)
+allow_location_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[1]')
+allow_location_button.click()
+notifications_button = driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div/div/div[3]/button[2]')
+notifications_button.click()
+cookies = driver.find_element_by_xpath('//*[@id="content"]/div/div[2]/div/div/div[1]/button')
+cookies.click()
+
+# Click like 100 times
+for n in range(100):
+    sleep(1)
+
+    try:
+        print("Liking!")
+        like_button = driver.find_element_by_xpath(
+            '//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/div[4]/button')
+        like_button.click()
+
+    except ElementClickInterceptedException:
+        try:
+            match_popup = driver.find_element_by_css_selector(".itsAMatch a")
+            match_popup.click()
+        except NoSuchElementException:
+            sleep(2)
+
+driver.quit()
